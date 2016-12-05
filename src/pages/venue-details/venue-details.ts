b7378810ef9d80d1a9cb51814ca0bf5e4e52b44b
Tim Walter <twwalter@gmail.com>
2016-12-05 14:34:59 +1100

    fine tine venue details

--- src/pages/venue-details/venue-details.ts
+++ src/pages/venue-details/venue-details.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { Venue } from '../../models/venue';
+import { Event } from '../../models/event';
 
 import { WdiconfVenues } from '../../providers/wdiconf-venues';
-
+import { WdiconfEvents } from '../../providers/wdiconf-events';
 
 /*
   Generated class for the VenueDetails page.
@@ -18,17 +19,22 @@ import { WdiconfVenues } from '../../providers/wdiconf-venues';
 })
 export class VenueDetailsPage {
   venue: Venue;
-  id: string;
+  venueEvents: Event[];
+  venue_id: number;
 
   ionViewDidLoad() {
     console.log('Hello UserDetails Page');
   }
-  constructor(public navCtrl: NavController, private navParams: NavParams, private wdiconfVenues: WdiconfVenues) {
-    this.id = navParams.get('id');
+  constructor(public navCtrl: NavController, private navParams: NavParams, private wdiconfVenues: WdiconfVenues, private wdiconfEvents: WdiconfEvents) {
+    this.venue_id = navParams.get('id');
     console.log(this.venue);
-    wdiconfVenues.loadDetails(this.id).subscribe(venue => {
+    wdiconfVenues.loadDetails(this.venue_id).subscribe(venue => {
       this.venue = venue[0];
       console.log(venue)
+    })
+    wdiconfEvents.loadForEvents(this.venue_id).subscribe(events => {
+      this.venueEvents = events;
+      console.log(events)
     })
   }
 
--- src/providers/wdiconf-venues.ts
+++ src/providers/wdiconf-venues.ts
@@ -27,10 +27,11 @@ export class WdiconfVenues {
   }
 
   // Get venue details user by venue ID
-  loadDetails(id: string): Observable<Venue> {
+  loadDetails(id: number): Observable<Venue> {
     return this.http.get(`${this.wdiconfVenuesApiUrl}/${id}`)
       .map(res => <Venue>(res.json().results));
   }
+
 
 
 }
