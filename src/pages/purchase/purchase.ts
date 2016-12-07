5345f0d55d8717c8a399ca9b256b16e02d7d020d
CKS <ckseng86@gmail.com>
2016-12-07 14:19:06 +1100

    add purchase function (working version)

--- src/pages/purchase/purchase.ts
+++ src/pages/purchase/purchase.ts
@@ -29,51 +29,30 @@ export class PurchasePage {
   logForm() {
     var data = {};
     var url = 'https://wdiconfapi.herokuapp.com/payment';
+    var self = this;
     Stripe.card.createToken(this.card, function(status, response) {
       if (response.error) {
         console.log("Error")
       }
       else {
         var token = response.id;
-        var token_pass = { stripeToken: token}
-
-        // new Promise(resolve => {
-        //     this.http.post('http://wdiconfapi.herokuapp.com/payment',).subscribe(data => {
-        //         if(data){
-        //           console.log(data.json())
-        //           if (data.json().success) {
-        //             // console.log()
-        //           }
-        //           resolve(true);
-        //         }
-        //         else
-        //           resolve(false);
-        //           return (data.json);
-        //     });
-        // });
-
-
-        var token = response.id;
-        var token_pass = { stripeToken: token}
-        var url = "http://wdiconfapi.herokuapp.com/payment"
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
-        this.http.post('url',
-          JSON.stringify(token_pass),
-          { headers: headers })
-          .map((res: Response) => {
-            console.log(res);
-            res.json()
-          })
-          .subscribe((res) => console.log("res", res));
-
-
-        // var my_form = document.createElement('form');
-        // var my_tb = document.createElement('input');
-        // my_tb.name = 'stripeToken';
-        // my_tb.value = token;
-        // my_form.appendChild(my_tb);
-        // this.http.post(url, my_form).map(res => res.json());
+        var token_obj = { stripeToken: token }
+        new Promise(resolve => {
+            self.http.post('http://localhost:3000/payment', token_obj, headers).subscribe(data => {
+                if(data){
+                  console.log(data.json())
+                  if (data.json().success) {
+                    
+                  }
+                  resolve(true);
+                }
+                else
+                  resolve(false);
+                  return (data.json);
+            });
+        });
       }
     });
   }
